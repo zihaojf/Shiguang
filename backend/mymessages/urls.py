03b06ede8399c70c4c86a784ab38020fb305c1bd
zihaojf <1845037747@qq.com>
2025-06-28 17:43:21 +0800

    消息模块添加获取联系列表和聊天详情接口

--- backend/mymessages/urls.py
+++ backend/mymessages/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r'', MessageViewSet, basename='mymessages')
+router.register(r'', MessageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
